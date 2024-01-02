3936fa5f3d24baed3e4b97be51b772ff7de5e7dc
Lahbiyeb-Ismail <ismaillhbibe@gmail.com>
2024-01-02 17:39:18 +0100

    python script that check the last_digit of a number

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,12 +2,25 @@
 import random
 
 number = random.randint(-10000, 10000)
-last_digit = str(number)[-1]
-output_str = "Last digit of " + str(number) + " is " + last_digit;
+sign = 1;
 
-if int(last_digit) > 5:
+if number < 0:
+	sign = -1
+else:
+	sign = 1
+
+last_digit = str(number)[-1]
+
+if sign == -1 and last_digit != "0":
+	last_digit = int(last_digit) * sign
+else:
+	last_digit = int(last_digit) * sign
+
+output_str = "Last digit of " + str(number) + " is " + str(last_digit);
+
+if last_digit  > 5:
 	output_str += " and is greater that 5"
-elif int(last_digit) == 0:
+elif last_digit == 0:
 	output_str += " and is 0"
 else:
 	output_str += " and is less than 6 and not 0"
