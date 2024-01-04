370a4089b4708770951885d2417bc76e067d6d43
Lahbiyeb-Ismail <ismaillhbibe@gmail.com>
2024-01-04 16:01:42 +0100

    python program that imports all functions from the file calculator_1.py and handles basic operations.

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,27 +1,23 @@
 #!/usr/bin/python3
+
 if __name__ == "__main__":
-    import sys
-
-    nargs = len(sys.argv) - 1
-    if nargs != 3:
-        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        sys.exit(1)
-
-    operator = sys.argv[2]
-    if operator != "+" and operator != "-" and operator != "*" and operator != "/":
-        print("Unknown operator. Available operators: +, -, * and /")
-        sys.exit(1)
+    from sys import argv
 
     from calculator_1 import add, div, mul, sub
 
-    a = int(sys.argv[1])
-    b = int(sys.argv[3])
+    argv_len = len(argv) - 1
 
-    if operator == "+":
-        print("{} + {} = {}".format(a, b, add(a, b)))
-    elif operator == "-":
-        print("{} - {} = {}".format(a, b, sub(a, b)))
-    elif operator == "*":
-        print("{} * {} = {}".format(a, b, mul(a, b)))
-    else:
-        print("{} / {} = {}".format(a, b, div(a, b)))
+    if argv_len != 3:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+
+    operations = {"+": add, "-": sub, "*": mul, "/": div}
+    operator = argv[2]
+
+    if operator not in list(operations.keys()):
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
+
+    a = int(argv[1])
+    b = int(argv[3])
+    print("{} {} {} = {}".format(a, operator, b, operations[operator](a, b)))
