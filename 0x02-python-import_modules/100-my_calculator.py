f8346132c088cd2438eda0468565e34b12e891f3
Lahbiyeb-Ismail <ismaillhbibe@gmail.com>
2024-01-04 15:49:35 +0100

    python program that imports all functions from the file calculator_1.py and handles basic operations.

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -2,8 +2,6 @@
 if __name__ == "__main__":
     from sys import argv
 
-    from calculator_1 import add, div, mul, sub
-
     argv_len = len(argv) - 1
     if argv_len != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
@@ -14,13 +12,15 @@ if __name__ == "__main__":
         print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
 
+    from calculator_1 import add, div, mul, sub
+
     a = int(argv[1])
     b = int(argv[3])
     if operator == "+":
-        print("{:d} {:s} {:d} = {:d}".format(a, operator, b, add(a, b)))
+        print("{} {} {} = {}".format(a, operator, b, add(a, b)))
     if operator == "-":
-        print("{:d} {:s} {:d} = {:d}".format(a, operator, b, sub(a, b)))
+        print("{} {} {} = {}".format(a, operator, b, sub(a, b)))
     if operator == "*":
-        print("{:d} {:s} {:d} = {:d}".format(a, operator, b, mul(a, b)))
+        print("{} {} {} = {}".format(a, operator, b, mul(a, b)))
     if operator == "/":
-        print("{:d} {:s} {:d} = {:d}".format(a, operator, b, div(a, b)))
+        print("{} {} {} = {}".format(a, operator, b, div(a, b)))
