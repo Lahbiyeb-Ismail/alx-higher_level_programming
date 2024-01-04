55b8ce39ffc08cd6ed9b247bbd4554301923c785
Lahbiyeb-Ismail <ismaillhbibe@gmail.com>
2024-01-04 15:50:42 +0100

    python program that imports all functions from the file calculator_1.py and handles basic operations.

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -16,6 +16,7 @@ if __name__ == "__main__":
 
     a = int(argv[1])
     b = int(argv[3])
+
     if operator == "+":
         print("{} {} {} = {}".format(a, operator, b, add(a, b)))
     if operator == "-":
