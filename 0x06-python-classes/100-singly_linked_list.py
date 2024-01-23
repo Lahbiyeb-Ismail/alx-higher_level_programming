431729cc3b485b3cb884a28c89ee77fe5559295c
Lahbiyeb-Ismail <ismaillhbibe@gmail.com>
2024-01-23 21:26:12 +0100

    A singly linked list class

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -1,74 +1,69 @@
 #!/usr/bin/python3
-"""
-A Node class
-"""
+"""Singly Linked List"""
 
 
 class Node:
+    """Represents a node in a singly linked list"""
+
     def __init__(self, data, next_node=None):
-        """initializes the node with instance variables"""
+        """Initialize node with data and link to next node"""
         self.data = data
         self.next_node = next_node
 
     @property
     def data(self):
+        """Get the data stored in the node"""
         return self.__data
 
     @data.setter
     def data(self, value):
+        """Set the data stored in the node
+        Data must be an integer"""
         if not isinstance(value, int):
             raise TypeError("data must be an integer")
         self.__data = value
 
     @property
     def next_node(self):
+        """Get the next node linked to this node"""
         return self.__next_node
 
     @next_node.setter
     def next_node(self, value):
-        if not isinstance(value, Node) and value is not None:
+        """Set the next node linked to this node
+        Must be a Node object or None"""
+        if value is not None and not isinstance(value, Node):
             raise TypeError("next_node must be a Node object")
         self.__next_node = value
 
 
-"""
-A Singly linked list class
-"""
-
-
 class SinglyLinkedList:
-    def __str__(self):
-        """make list printable"""
-        rtn = ""
-        ptr = self.__head
-
-        while ptr is not None:
-            rtn += str(ptr.data)
-            if ptr.next_node is not None:
-                rtn += "\n"
-            ptr = ptr.next_node
-
-        return rtn
+    """Singly linked list data structure"""
 
     def __init__(self):
-        """Initializes the singly linked list"""
-        self.__head = None
+        """Create empty linked list"""
+        self.head = None
+
+    def __str__(self):
+        """Print entire linked list"""
+        values = []
+        node = self.head
+        while node is not None:
+            values.append(str(node.data))
+            node = node.next_node
+        return "\n".join(values)
 
     def sorted_insert(self, value):
-        """insert in a sorted fashion
-        Args:
-            value: what the value will be on the node
-        """
-        ptr = self.__head
-
-        while ptr is not None:
-            if ptr.data > value:
-                break
-            ptr_prev = ptr
-            ptr = ptr.next_node
-
-        newNode = Node(value, ptr)
-        if ptr == self.__head:
-            self.__head = newNode
+        """Insert node with specified value in sorted order"""
+        new_node = Node(value)
+        if self.head is None or self.head.data >= new_node.data:
+            new_node.next_node = self.head
+            self.head = new_node
         else:
-            ptr_prev.next_node = newNode
+            current = self.head
+            while (
+                current.next_node is not None and current.next_node.data < new_node.data
+            ):
+                current = current.next_node
+            new_node.next_node = current.next_node
+            current.next_node = new_node
