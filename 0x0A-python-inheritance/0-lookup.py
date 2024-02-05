bc5c19599d6694b070ab66ccb2fa754bf0374b25
Lahbiyeb-Ismail <ismaillhbibe@gmail.com>
2024-02-05 15:06:12 +0100

    C function that returns the list of available attributes and methods of an object

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,5 +1,17 @@
 #!/usr/bin/python3
 
+"""
+Function that returns the list of available attributes
+and methods of an object
+"""
+
 
 def lookup(obj):
-    return dir(obj)
+    """
+    lookup Function
+    Args:
+        param1(obj)
+    Return:
+        list of all attributes
+    """
+    return list(dir(obj))
