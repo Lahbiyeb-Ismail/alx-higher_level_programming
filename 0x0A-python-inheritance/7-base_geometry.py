9ab803e73ba90b8790fe0323002cd49da6266d20
Lahbiyeb-Ismail <ismaillhbibe@gmail.com>
2024-02-05 16:43:11 +0100

    adding the integer_validator method to the BaseGeometry class

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,70 @@
+#!/usr/bin/python3
+"""BaseGeometry class"""
+
+
+class BaseGeometry:
+    """
+    A base class for geometrical shapes.
+
+    This class defines a placeholder method for calculating the area
+    of geometrical shapes. Subclasses are expected to implement the
+    `area()` method according to the specific geometry.
+
+    Methods:
+    --------
+    area():
+        Calculate the area of the geometrical shape.
+                Subclasses must implement this method.
+
+    integer_validator(name, value):
+        Validate that a given value is a positive integer.
+
+    Raises:
+    -------
+    Exception:
+        This exception is raised if the `area()` method is not
+                implemented in a subclass.
+
+    TypeError:
+        This exception is raised if the provided value is not
+                an integer in the `integer_validator` method.
+
+    ValueError:
+        This exception is raised if the provided value is
+                less than or equal to 0 in the `integer_validator`
+                                method.
+    """
+
+    def area(self):
+        """
+        Placeholder method for calculating the area of geometrical shapes.
+
+        Raises:
+        -------
+        Exception:
+            This exception is raised if the `area()` method is not implemented
+            in a subclass.
+        """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """
+        Validate that a given value is a positive integer.
+
+        Parameters:
+        -----------
+        name (str): The name of the value being validated.
+        value (int): The value to be validated.
+
+        Raises:
+        -------
+        TypeError:
+            If the provided value is not an integer.
+
+        ValueError:
+            If the provided value is less than or equal to 0.
+        """
+        if not isinstance(value, int):
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
