360b3aecf3dfb791c26fc6889e0e523f5995e1bd
Lahbiyeb-Ismail <ismaillhbibe@gmail.com>
2024-02-09 17:24:32 +0100

    Adding the update method that assigns attributes

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -65,3 +65,19 @@ class Square(Rectangle):
         self.validate_att("width", value)
         self.width = value
         self.height = value
+
+    def update(self, *args, **kwargs):
+        """
+        Updates the square with the given attributes
+        """
+        if len(args) > 0:
+            self.id = args[0]
+        if len(args) > 1:
+            self.size = args[1]
+        if len(args) > 2:
+            self.x = args[2]
+        if len(args) > 3:
+            self.y = args[3]
+        if kwargs and len(args) == 0:
+            for key, val in kwargs.items():
+                setattr(self, key, val)
