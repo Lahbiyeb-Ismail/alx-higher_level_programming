66d0f44d67997738407f8ea233beeabcb4c6095a
Lahbiyeb-Ismail <ismaillhbibe@gmail.com>
2024-02-12 19:02:46 +0100

    adding test files

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -1,109 +1,97 @@
 #!/usr/bin/python3
-import json
+""" Module for test Base class """
 import unittest
+from io import StringIO
+from unittest import TestCase
+from unittest.mock import patch
 
 from models.base import Base
+from models.rectangle import Rectangle
 from models.square import Square
 
-"""
-    Creating test cases for the base module
-"""
 
+class TestBaseMethods(unittest.TestCase):
+    """Suite to test Base class"""
 
-class test_base(unittest.TestCase):
-    """
-    Testing base
-    """
-
-    def test_id_none(self):
-        """
-        Sending no id
-        """
-        b = Base()
-        self.assertEqual(1, b.id)
+    def setUp(self):
+        """Method invoked for each test"""
+        Base._Base__nb_objects = 0
 
     def test_id(self):
-        """
-        Sending a valid id
-        """
-        b = Base(50)
-        self.assertEqual(50, b.id)
+        """Test assigned id"""
+        new = Base(1)
+        self.assertEqual(new.id, 1)
 
-    def test_id_zero(self):
-        """
-        Sending an id 0
-        """
-        b = Base(0)
-        self.assertEqual(0, b.id)
+    def test_id_default(self):
+        """Test default id"""
+        new = Base()
+        self.assertEqual(new.id, 1)
 
-    def test_id_negative(self):
-        """
-        Sending a negative id
-        """
-        b = Base(-20)
-        self.assertEqual(-20, b.id)
+    def test_id_nb_objects(self):
+        """Test nb object attribute"""
+        new = Base()
+        new2 = Base()
+        new3 = Base()
+        self.assertEqual(new.id, 1)
+        self.assertEqual(new2.id, 2)
+        self.assertEqual(new3.id, 3)
 
-    def test_id_string(self):
-        """
-        Sending an id that is not an int
-        """
-        b = Base("Betty")
-        self.assertEqual("Betty", b.id)
+    def test_id_mix(self):
+        """Test nb object attributes and assigned id"""
+        new = Base()
+        new2 = Base(1024)
+        new3 = Base()
+        self.assertEqual(new.id, 1)
+        self.assertEqual(new2.id, 1024)
+        self.assertEqual(new3.id, 2)
 
-    def test_id_list(self):
-        """
-        Sending an id that is not an int
-        """
-        b = Base([1, 2, 3])
-        self.assertEqual([1, 2, 3], b.id)
+    def test_string_id(self):
+        """Test string id"""
+        new = Base("1")
+        self.assertEqual(new.id, "1")
 
-    def test_id_dict(self):
-        """
-        Sending an id that is not an int
-        """
-        b = Base({"id": 109})
-        self.assertEqual({"id": 109}, b.id)
+    def test_more_args_id(self):
+        """Test passing more args to init method"""
+        with self.assertRaises(TypeError):
+            new = Base(1, 1)
 
-    def test_id_tuple(self):
-        """
-        Sending an id that is not an int
-        """
-        b = Base((8,))
-        self.assertEqual((8,), b.id)
+    def test_access_private_attrs(self):
+        """Test accessing to private attributes"""
+        new = Base()
+        with self.assertRaises(AttributeError):
+            new.__nb_objects
 
-    def test_to_json_type(self):
-        """
-        Testing the json string
-        """
-        sq = Square(1)
-        json_dict = sq.to_dictionary()
-        json_string = Base.to_json_string([json_dict])
-        self.assertEqual(type(json_string), str)
+    def test_save_to_file_1(self):
+        """Test JSON file"""
+        Square.save_to_file(None)
+        res = "[]\n"
+        with open("Square.json", "r") as file:
+            with patch("sys.stdout", new=StringIO()) as str_out:
+                print(file.read())
+                self.assertEqual(str_out.getvalue(), res)
 
-    def test_to_json_value(self):
-        """
-        Testing the json string
-        """
-        sq = Square(1, 0, 0, 609)
-        json_dict = sq.to_dictionary()
-        json_string = Base.to_json_string([json_dict])
-        self.assertEqual(
-            json.loads(json_string), [{"id": 609, "y": 0, "size": 1, "x": 0}]
-        )
+        try:
+            os.remove("Square.json")
+        except:
+            pass
 
-    def test_to_json_None(self):
-        """
-        Testing the json string
-        """
-        sq = Square(1, 0, 0, 609)
-        json_dict = sq.to_dictionary()
-        json_string = Base.to_json_string(None)
-        self.assertEqual(json_string, "[]")
+        Square.save_to_file([])
+        with open("Square.json", "r") as file:
+            self.assertEqual(file.read(), "[]")
 
-    def test_to_json_Empty(self):
-        """
-        Testing the json string
-        """
-        sq = Square(1, 0, 0, 609)
-        json_string = Base.to_json_string([])
-        self.assertEqual(json_string, "[]")
+    def test_save_to_file_2(self):
+        """Test JSON file"""
+        Rectangle.save_to_file(None)
+        res = "[]\n"
+        with open("Rectangle.json", "r") as file:
+            with patch("sys.stdout", new=StringIO()) as str_out:
+                print(file.read())
+                self.assertEqual(str_out.getvalue(), res)
+        try:
+            os.remove("Rectangle.json")
+        except:
+            pass
+
+        Rectangle.save_to_file([])
+        with open("Rectangle.json", "r") as file:
+            self.assertEqual(file.read(), "[]")
