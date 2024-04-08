65e1a4daeec67d70f06ebb49099a502220314432
Lahbiyeb-Ismail <ismaillhbibe@gmail.com>
2024-04-08 16:08:27 +0000

    script that computes and prints a factorial

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const argv = process.argv;
+
+function factorial(n) {
+  if (n === 1 || typeof n === NaN) return 1;
+
+  return n * factorial(n - 1);
+}
+
+console.log(factorial(+argv[2]));
