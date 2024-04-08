bfe7410aa5609eb4f661ad676c807b14c9066021
Lahbiyeb-Ismail <ismaillhbibe@gmail.com>
2024-04-08 15:35:01 +0000

    script that prints a square

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -3,9 +3,16 @@
 const argv = process.argv;
 
 if (+argv[2]) {
+  let square = '';
   for (let i = 0; i < +argv[2]; i++) {
-    for (let j = 0; j < +argv[2]; j++) console.log('#');
+    for (let j = 0; j < +argv[2]; j++) {
+      square += 'X';
+    }
+
+    if (i < +argv[2] - 1) square += '\n';
   }
+
+  console.log(square);
 } else {
   console.log('Missing size');
 }
