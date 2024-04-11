6a86b60883ac889bfb16b32bcbeb6278b597953d
Lahbiyeb-Ismail <ismaillhbibe@gmail.com>
2024-04-11 18:06:57 +0000

    script that prints the first State object from the database hbtn_0e_6_usa

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""script that lists all State objects from the database hbtn_0e_6_usa
+"""
+import sys
+from model_state import Base, State
+
+from sqlalchemy import (create_engine, orm)
+
+if __name__ == "__main__":
+    user = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+
+    db_link = 'mysql+mysqldb://{}:\
+              {}@localhost/{}'.format(user, password, db_name)
+
+    engine = create_engine(db_link, pool_pre_ping=True)
+
+    Base.metadata.create_all(engine)
+
+    session = orm.Session(engine)
+
+    first_state = session.query(State).order_by(State.id).first()
+
+    if first_state:
+        print("{}: {}".format(first_state.id, first_state.name))
+    else:
+        print("Nothing")
+
+    session.close()
