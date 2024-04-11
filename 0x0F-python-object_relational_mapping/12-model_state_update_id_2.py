3dd936401c158ff78ea5fe78875c495b652d274e
Lahbiyeb-Ismail <ismaillhbibe@gmail.com>
2024-04-11 18:50:32 +0000

    script that changes the name of a State object from the database hbtn_0e_6_usa

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""script that lists all State objects from the database hbtn_0e_6_usa
+"""
+import sys
+from model_state import Base, State
+
+from sqlalchemy import (create_engine, orm)
+
+if __name__ == "__main__":
+    user = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+
+    db_link = 'mysql+mysqldb://{}:\
+              {}@localhost/{}'.format(user, password, db_name)
+
+    engine = create_engine(db_link, pool_pre_ping=True)
+
+    Base.metadata.create_all(engine)
+
+    session = orm.Session(engine)
+
+    state = session.query(State).filter(State.id == 2).first()
+
+    if state:
+        state.name = "New Mexico"
+        session.commit()
+
+    session.close()
