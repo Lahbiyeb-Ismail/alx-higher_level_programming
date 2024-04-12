e6c72869e92c7cf07eaeb3b38709db79a76b081d
Lahbiyeb-Ismail <ismaillhbibe@gmail.com>
2024-04-12 15:53:45 +0000

    script that deletes all State objects with a name containing the letter a from the database hbtn_0e_6_usa

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -2,25 +2,28 @@
 """script that lists all State objects from the database hbtn_0e_6_usa
 """
 import sys
-from model_state import Base, State
 
-from sqlalchemy import (create_engine, orm)
+from sqlalchemy import create_engine, orm
+
+from model_state import Base, State
 
 if __name__ == "__main__":
     user = sys.argv[1]
-    password = sys.argv[2]
+    pwd = sys.argv[2]
     db_name = sys.argv[3]
 
-    db_link = 'mysql+mysqldb://{}:\
-              {}@localhost/{}'.format(user, password, db_name)
+    db_url = "mysql+mysqldb://{}:{}@localhost/{}".format(user, pwd, db_name)
 
-    engine = create_engine(db_link, pool_pre_ping=True)
+    engine = create_engine(db_url, pool_pre_ping=True)
 
     Base.metadata.create_all(engine)
 
     session = orm.Session(engine)
 
-    session.query(State).delete(State.name.like("%a%"))
+    state_with_a = session.query(State).filter(State.name.like("%a%")).all()
+
+    for state in state_with_a:
+        session.delete(state)
 
     session.commit()
 
