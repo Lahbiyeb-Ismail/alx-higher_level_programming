182196515fff2a0fbc16402ac480029cb533e05f
Lahbiyeb-Ismail <ismaillhbibe@gmail.com>
2024-04-12 17:20:21 +0000

    script that lists all State objects, and corresponding City objects, contained in the database hbtn_0e_101_usa

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -27,7 +27,7 @@ if __name__ == "__main__":
         print("{}: {}".format(state.id, state.name))
 
         for city in state.cities:
-            print("   {}: {}".format(city.id, city.name))
+            print("    {}: {}".format(city.id, city.name))
 
     session.commit()
     session.close()
