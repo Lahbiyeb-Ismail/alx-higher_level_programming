00d388defd065e0257b9f0c9177938e8b5275067
Lahbiyeb-Ismail <ismaillhbibe@gmail.com>
2024-04-25 14:34:03 +0100

    Refactor find_peak function in 6-peak.py for better readability and maintainability

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -3,18 +3,18 @@
 # Function that finds a peak in a list of unsorted integers.
 
 def find_peak(list_of_integers):
-  if not list_of_integers:
-    return None
+    if not list_of_integers:
+        return None
 
-  low = 0
-  high = len(list_of_integers) - 1
+    low = 0
+    high = len(list_of_integers) - 1
 
-  while low < high:
-    mid = (low + high) // 2
+    while low < high:
+        mid = (low + high) // 2
 
-    if list_of_integers[mid] > list_of_integers[mid + 1]:
-      high = mid
-    else:
-      low = mid + 1
+        if list_of_integers[mid] > list_of_integers[mid + 1]:
+            high = mid
+        else:
+            low = mid + 1
 
-  return list_of_integers[low]
+    return list_of_integers[low]
