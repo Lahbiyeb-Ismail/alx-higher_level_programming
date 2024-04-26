fd3b3b6742e4c01cd6749f235db811698c5f1984
Lahbiyeb-Ismail <ismaillhbibe@gmail.com>
2024-04-26 14:13:45 +0100

    Refactor 2-post_email.py script to improve readability and add docstring

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python3
 
-""""""
+"""
+Python script that takes in a URL and an email, sends a POST
+request to the passed URL with the email as a parameter, and
+displays the body of the response (decoded in utf-8)
+"""
 
+from sys import argv
 import urllib.parse
 import urllib.request
 
 if __name__ == "__main__":
-    from sys import argv
-
     url = argv[1]
     value = {'email': argv[2]}
     data = urllib.parse.urlencode(value)
